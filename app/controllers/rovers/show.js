18f9de919fa647c9a8c1169b6e8bb4c8935e6b79
Chris C Cerami <chrisccerami@gmail.com>
2015-07-11 17:22:40 -0400

    Implement search for photos

--- app/controllers/rovers/show.js
+++ app/controllers/rovers/show.js
@@ -4,5 +4,19 @@ export default Ember.Controller.extend({
   queryParams: ['sol', 'earth_date', 'camera'],
   sol: null,
   earth_date: null,
-  camera: null
+  camera: null,
+
+  actions: {
+    photoQuery: function() {
+      var _this = this;
+      var sol = this.get('sol');
+      var camera = this.get('camera');
+      var url = '/api/v1/rovers/' + this.rover.name;
+      url += '/photos?sol=' + sol;
+      url += '&camera=' + camera;
+      Ember.$.getJSON(url, function(response) {
+        _this.set('photos', response.photos);
+      });
+    }
+  }
 });
