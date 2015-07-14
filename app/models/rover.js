765068390a2a367d012fa39e530c4e4ba0ea509f
Chris C Cerami <chrisccerami@gmail.com>
2015-07-14 01:51:55 -0400

    Fix max_date attribute in rover model

--- app/models/rover.js
+++ app/models/rover.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   landing_date: DS.attr('date'),
   max_sol: DS.attr('number'),
-  max_data: DS.attr('date'),
+  max_date: DS.attr('date'),
   total_photos: DS.attr('number'),
   cameras: DS.hasMany('camera', { async: true }),
   photos: DS.hasMany('photo', { async: true }),
